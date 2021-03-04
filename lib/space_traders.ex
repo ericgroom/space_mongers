75c55d15a2b8bcc1ef621bcb0b508654604748e7
Eric Groom <bcgroom@gmail.com>
2021-03-04 08:52:24 -0800

    get current user

--- lib/space_traders.ex
+++ lib/space_traders.ex
@@ -3,11 +3,16 @@ defmodule SpaceTraders do
 
   plug Tesla.Middleware.BaseUrl, "https://api.spacetraders.io"
   plug Tesla.Middleware.JSON
+  plug Tesla.Middleware.Query, [token: @token]
 
   @username Application.fetch_env!(:space_traders, :username)
   @token Application.fetch_env!(:space_traders, :token)
 
   def status do
     get("/game/status")
+  end
+
+  def current_user do
+    get("/users/" <> @username)
   end
 end
