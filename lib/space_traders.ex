1beb6a6435cf060395891814de1813bf666b5907
Eric Groom <bcgroom@gmail.com>
2021-03-04 08:58:27 -0800

    unwrap body

--- lib/space_traders.ex
+++ lib/space_traders.ex
@@ -9,10 +9,15 @@ defmodule SpaceTraders do
   @token Application.fetch_env!(:space_traders, :token)
 
   def status do
-    get("/game/status")
+    get("/game/status") |> unwrap()
   end
 
   def current_user do
-    get("/users/" <> @username)
+    get("/users/" <> @username) |> unwrap()
+  end
+
+  defp unwrap(response) do
+    {:ok, env} = response
+    env.body
   end
 end
