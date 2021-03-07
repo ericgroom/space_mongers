ae9d52dd1a39360d047e990b7b2882bc6be2f22a
Eric Groom <bcgroom@gmail.com>
2021-03-06 16:02:35 -0800

    config logger

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,9 @@ import Config
 #   username: "replace in other env",
 #   token: "replace in other env"
 
+config :logger,
+  level: :info
+
 config :tesla, adapter: Tesla.Adapter.Hackney
 
 config :tesla, Tesla.Middleware.Logger,
