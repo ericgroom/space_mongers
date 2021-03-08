ef5a2285d39291df030219be3303856c9019d968
Eric Groom <bcgroom@gmail.com>
2021-03-07 16:19:31 -0800

    remove tesla logger

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,3 @@ config :logger,
   level: :info
 
 config :tesla, adapter: Tesla.Adapter.Hackney
-
-config :tesla, Tesla.Middleware.Logger,
-  debug: false,
-  filter_headers: ["authorization"]
--- lib/space_mongers/api_client.ex
+++ lib/space_mongers/api_client.ex
@@ -14,7 +14,6 @@ defmodule SpaceMongers.ApiClient do
   @spec new(String.t(), String.t()) :: t()
   def new(username, token) do
     middleware = [
-      Tesla.Middleware.Logger,
       {Tesla.Middleware.BaseUrl, "https://api.spacetraders.io"},
       Tesla.Middleware.JSON,
       {Tesla.Middleware.Headers, [{"Authorization", "Bearer #{token}"}]},
