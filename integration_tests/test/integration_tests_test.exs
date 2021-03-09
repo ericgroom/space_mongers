a9ca8a2770a06b866f371011bd44f170fc202899
Eric Groom <bcgroom@gmail.com>
2021-03-08 20:01:17 -0800

    add test for taking out a loan

--- integration_tests/test/integration_tests_test.exs
+++ integration_tests/test/integration_tests_test.exs
@@ -3,15 +3,24 @@ defmodule IntegrationTests do
 
   describe "SpaceMongers" do
     test "can get server status" do
-      assert {:ok, user_info} = SpaceMongers.claim_username(Utils.random_username())
-      %{"user" => %{"username" => username}, "token" => token} = user_info
-      client = SpaceMongers.ApiClient.new(username, token)
+      client = make_client()
       assert {:ok, "spacetraders is currently online and available to play"} = SpaceMongers.status(client)
     end
 
     test "can create user" do
       username = Utils.random_username()
       assert {:ok, %{"user" => %{"username" => ^username}}} = SpaceMongers.claim_username(username)
+    end
+
+    test "can take out a loan" do
+      client = make_client()
+      assert {:ok, _loan} = SpaceMongers.buy_loan(client, "STARTUP")
+    end
+
+    defp make_client() do
+      assert {:ok, user_info} = SpaceMongers.claim_username(Utils.random_username())
+      %{"user" => %{"username" => username}, "token" => token} = user_info
+      SpaceMongers.ApiClient.new(username, token)
     end
   end
 end
