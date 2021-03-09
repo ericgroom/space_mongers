7d4e9e506c6ac1be37fa006d306440e92b034a2e
Eric Groom <bcgroom@gmail.com>
2021-03-08 19:57:52 -0800

    test status

--- integration_tests/test/integration_tests_test.exs
+++ integration_tests/test/integration_tests_test.exs
@@ -2,6 +2,13 @@ defmodule IntegrationTests do
   use ExUnit.Case
 
   describe "SpaceMongers" do
+    test "can get server status" do
+      assert {:ok, user_info} = SpaceMongers.claim_username(Utils.random_username())
+      %{"user" => %{"username" => username}, "token" => token} = user_info
+      client = SpaceMongers.ApiClient.new(username, token)
+      assert {:ok, "spacetraders is currently online and available to play"} = SpaceMongers.status(client)
+    end
+
     test "can create user" do
       username = Utils.random_username()
       assert {:ok, %{"user" => %{"username" => ^username}}} = SpaceMongers.claim_username(username)
