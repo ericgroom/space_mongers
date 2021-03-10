3dcf0323c63a639ea6e8cd4b3c48697c240f0084
Eric Groom <bcgroom@gmail.com>
2021-03-10 08:38:30 -0800

    move response formatting functions

--- lib/space_mongers.ex
+++ lib/space_mongers.ex
@@ -9,6 +9,7 @@ defmodule SpaceMongers do
   banned. Right now the rate limiting is quite agressive so expect SpaceMongers to become more efficient in the future.
   """
   alias SpaceMongers.{ApiClient, FullResponse, SpaceTraders, Models, Parsers}
+  import SpaceMongers.Formatters, only: [format_response: 3]
 
   @type client() :: ApiClient.t()
   @type response(t) :: {:ok, t} | {:ok, FullResponse.t()} | {:error, any()}
@@ -260,28 +261,4 @@ defmodule SpaceMongers do
           |> Parsers.parse_order()
       end, opts)
   end
-
-  defp format_response(result, extract_success, opts)  do
-    skip_deserialization = Keyword.get(opts, :skip_deserialization, false)
-    case result do
-      {:ok, response} ->
-        if response.status >= 400 do
-          error_response(response, skip_deserialization)
-        else
-          success_response(response, extract_success, skip_deserialization)
-        end
-      {:error, env} ->
-        error_response(env, skip_deserialization)
-    end
-  end
-
-  defp success_response(full_response, deserialize, skip_deserialization)
-  defp success_response(response, _deserialize, true), do: {:ok, response}
-  defp success_response(response, deserialize, false), do: {:ok, deserialize.(response)}
-
-  defp error_response(full_response, skip_deserialization)
-  defp error_response(response, true), do: {:error, response}
-  defp error_response(%{body: %{"error" => %{"message" => message}}}, false), do: {:error, message}
-  defp error_response(%{body: body}, false), do: {:error, body}
-  defp error_response(reason, _), do: {:error, reason}
 end
