ad289a1fe2f345295bbfc2c560e326aadbcbe574
Eric Groom <bcgroom@gmail.com>
2021-03-10 08:28:26 -0800

    replace include_full_response with skip_deserialization

--- lib/space_mongers.ex
+++ lib/space_mongers.ex
@@ -11,8 +11,8 @@ defmodule SpaceMongers do
   alias SpaceMongers.{ApiClient, FullResponse, SpaceTraders, Models, Parsers}
 
   @type client() :: ApiClient.t()
-  @type response(t) :: {:ok, t} | {:error, any()} | {:ok, t, FullResponse.t()} | {:error, any(), FullResponse.t()}
-  @type options() :: [include_full_response: boolean()]
+  @type response(t) :: {:ok, t} | {:ok, FullResponse.t()} | {:error, any()}
+  @type options() :: [skip_deserialization: boolean()]
 
   @default_system "OE"
 
@@ -262,28 +262,26 @@ defmodule SpaceMongers do
   end
 
   defp format(result, extract_success, opts)  do
+    skip_deserialization = Keyword.get(opts, :skip_deserialization, false)
     case result do
       {:ok, response} ->
         if response.status >= 400 do
-          error_response(response)
-            |> include_response_if_needed(response, opts)
+          error_response(response, skip_deserialization)
         else
-          {:ok, extract_success.(response)}
-            |> include_response_if_needed(response, opts)
+          success_response(response, extract_success, skip_deserialization)
         end
       {:error, env} ->
-        error_response(env)
-          |> include_response_if_needed(env, opts)
+        error_response(env, skip_deserialization)
     end
   end
 
-  defp error_response(%{body: %{"error" => %{"message" => message}}}), do: {:error, message}
-  defp error_response(%{body: body}), do: {:error, body}
-  defp include_response_if_needed({result, value}, response, opts) do
-    if Keyword.get(opts, :include_full_response, false) do
-      {result, value, response}
-    else
-      {result, value}
-    end
-  end
+  defp success_response(full_response, deserialize, skip_deserialization)
+  defp success_response(response, _deserialize, true), do: {:ok, response}
+  defp success_response(response, deserialize, false), do: {:ok, deserialize.(response)}
+
+  defp error_response(full_response, skip_deserialization)
+  defp error_response(response, true), do: {:error, response}
+  defp error_response(%{body: %{"error" => %{"message" => message}}}, false), do: {:error, message}
+  defp error_response(%{body: body}, false), do: {:error, body}
+  defp error_response(reason, _), do: {:error, reason}
 end
