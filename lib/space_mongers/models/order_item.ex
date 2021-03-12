0d257c7ec618d5b9d0244663995b721ca1f0c325
Eric Groom <bcgroom@gmail.com>
2021-03-11 21:24:33 -0800

    fix type issue found by dialyzer

--- lib/space_mongers/models/order_item.ex
+++ lib/space_mongers/models/order_item.ex
@@ -4,7 +4,7 @@ defmodule SpaceMongers.Models.Order do
   """
   use SpaceMongers.Model,
     credits: integer(),
-    order: [Models.OrderItem.t()],
+    order: [Models.Order.OrderItem.t()],
     ship: Models.OwnedShip.t(),
     extra_fields: map()
 end
